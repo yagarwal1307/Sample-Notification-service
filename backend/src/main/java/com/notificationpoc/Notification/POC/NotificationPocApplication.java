cc54e7b24d12d18b26a3c935bafa555a087448f7
ShashwatMDas <shashwatmdas@gmail.com>
2021-06-14 19:10:54 +0530

    Backend initialized

---
+++ backend/src/main/java/com/notificationpoc/Notification/POC/NotificationPocApplication.java
@@ -0,0 +1,13 @@
+package com.notificationpoc.Notification.POC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationPocApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/notificationpoc/Notification/POC/NotificationPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificationpoc.Notification.POC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
