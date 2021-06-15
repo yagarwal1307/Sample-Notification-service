c884136fdc8b731b90338ab1a5d44bc1404d8746
ShashwatMDas <shashwatmdas@gmail.com>
2021-06-15 15:47:38 +0530

    Gradle and groovy backend initalized

---
+++ Notifications/src/main/groovy/com/notifications/Notifications/NotificationsApplication.groovy
@@ -0,0 +1,13 @@
+package com.notifications.Notifications
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class NotificationsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(NotificationsApplication, args)
+	}
+
+}
